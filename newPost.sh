53c3610edeb917e5baedd5be86646fe9688ee582
Andreas Neukoetter <andreas@omni-mad.com>
2013-05-10 14:03:41 +0200

    : Tweaked filename creation.

--- newPost.sh
+++ newPost.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
 title=$1
-postfile=_posts/`date -I`-${title}.markdown
-touch _posts/`date -I`-${title}.markdown
+cleantitle=${title//[ .]/_}
+echo $cleantitle
+d=`date "+%Y-%m-%d"`
+postfile=_posts/${d}-${cleantitle}.markdown
+
+if [ ! -f "${postfile}" ]
+then
+
+touch ${postfile}
 
 echo "---" >$postfile
 echo "layout: post" >>$postfile
@@ -10,4 +17,8 @@ echo "title: \"${title}\"" >>$postfile
 echo "time: \"18:50\"" >>$postfile
 echo "---" >>$postfile
 echo "" >>$postfile
+
+else
+	echo "ERROR: ${postfile} already exists."
+fi
 
